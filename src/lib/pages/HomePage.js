a7a2a49c78c9e110337158bde0094f2dbbd2021b
Hwang-se-woong <hsodragon@naver.com>
2019-08-24 00:12:45 +0900

    충돌 수정

--- src/lib/pages/HomePage.js
+++ src/lib/pages/HomePage.js
@@ -5,17 +5,13 @@ import MapComponent from "../components/MapComponent";
 import FooterComponent from "../components/FooterComponent";
 import MenuSettingComponent from "../components/MenuSettingComponent";
 import GeographyComponent from "../components/GeographyComponent";
-<<<<<<< HEAD
-=======
-import MyInfo from '../info/MyInfo';
->>>>>>> 257e5a3fe04612522950268e1894328247926495
+import MyInfo from "../info/MyInfo";
 
 const HomePage = props => {
-
   const profile = MyInfo.getProfile("profile");
   return (
     <>
-      <HeaderComponent  {...profile.state.header}/>
+      <HeaderComponent {...profile.state.header} />
       <MapComponent />
       {/* <GeographyComponent /> */}
       <FooterComponent path="/" />
