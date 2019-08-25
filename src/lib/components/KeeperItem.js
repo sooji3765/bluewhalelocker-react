d84b43a92b80233842b3cf33d95f57ce0b24039d
Hwang-se-woong <hsodragon@naver.com>
2019-08-26 00:12:06 +0900

    KeeperItem 컴포넌트 추가

---
+++ src/lib/components/KeeperItem.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+export default class KeeperItem extends Component {
+  render() {
+    const {
+      businessName,
+      phoneNumber,
+      currentLocker,
+      maxLocker,
+      keeperImg,
+      id,
+      onToggle
+    } = this.props;
+
+    return (
+      <div>
+        <li className="menu-item">
+          <a href="/keeper_info">
+            <div className="menu- keeper-item">
+              <div className="menu-keeper-img">
+                <img className="keeper-img" src="img/user2.png" alt="user2" />
+              </div>
+              <div className="menu-keeper-content">
+                <span className="keeper-name">{businessName}</span>
+                <span className="keeper-phone">
+                  {phoneNumber} <i className="fas fa-phone-alt" />
+                </span>
+              </div>
+              <div className="menu-keeper-count">
+                <span className="locker-count">
+                  {currentLocker}/{maxLocker}
+                </span>
+              </div>
+            </div>
+          </a>
+        </li>
+      </div>
+    );
+  }
+}
